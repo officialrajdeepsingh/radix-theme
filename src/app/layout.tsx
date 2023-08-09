5628c9158096926774f35cec7c8441f7051e7dc7
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-09 23:22:45 +0530

    adding theme in nextjs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
+import '@radix-ui/themes/styles.css';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <body className={inter.className}>
+<Theme appearance="dark">
+        {children}
+</Theme>
+        </body>
     </html>
   )
 }
