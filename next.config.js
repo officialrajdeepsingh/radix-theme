56fb165ee9fece4d6b172f1d0d61222b83662b8e
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-11 20:54:15 +0530

    update the nextjs config for github pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 const nextConfig = {
+  output:  process.env.NODE_ENV !== "production" ? undefined: "export",
   images: {
+    unoptimized: envImageUnoptimize,
     remotePatterns: [
       {
         hostname: "images.unsplash.com",
