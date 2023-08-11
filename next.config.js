33ea3b807e75cda5ad7430d7b1ca6f098f86a69c
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-11 13:55:34 +0530

    image path name config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
