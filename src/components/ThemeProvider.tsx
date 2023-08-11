358801e7f3b3e2f1c6db9b55e9acbab27d9f978a
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-11 20:30:53 +0530

    adding typescript type

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,7 +1,8 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
+import { ReactNode } from 'react';
 
-export function NextThemeProvider({ children }) {
+export function NextThemeProvider({ children }:{children:ReactNode}) {
   return <ThemeProvider attribute="class">
     {children}
   </ThemeProvider>;
