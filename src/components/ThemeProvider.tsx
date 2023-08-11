f9dde56fcade5c3f90c0f090f1a5caa56eb2caf9
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-11 13:55:19 +0530

    theme provide for dark and light mode

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,8 @@
+'use client';
+import { ThemeProvider } from 'next-themes';
+
+export function NextThemeProvider({ children }) {
+  return <ThemeProvider attribute="class">
+    {children}
+  </ThemeProvider>;
+}
