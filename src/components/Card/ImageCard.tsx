364155a4a3b8aeef7a0e8f02ecf5405cf262d57c
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-11 13:54:54 +0530

    card image component for home page

---
+++ src/components/Card/ImageCard.tsx
@@ -0,0 +1,54 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import {
+  AspectRatio,
+  Box,
+  Button,
+  Card,
+  Flex,
+  Heading,
+  Inset,
+  Text,
+} from "@radix-ui/themes";
+import Image from "next/image";
+export function ImageCard(
+  { variant }: { variant: "ghost" | "classic" | "surface" },
+) {
+  return (
+    <Card variant={variant} my={"8"} size="3">
+      <Flex direction={"column"} justify={"center"}>
+        <Box>
+          <Inset side="top" p="0">
+            <AspectRatio ratio={16 / 8}>
+              <Image
+                src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
+                alt="A house in a forest"
+                width={724}
+                height={500}
+                style={{
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                  padding: "1px",
+                  borderRadius: "var(--radius-2)",
+                }}
+              />
+            </AspectRatio>
+          </Inset>
+        </Box>
+
+        <Box mt={"4"}>
+          <Heading as="h2" color="gray" mb="1" size="5">
+            Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint
+            cupidatat.
+          </Heading>
+          <Text as="p" size="2">
+            Qui minim labore adipisicing minim sint cupidatat
+          </Text>
+          <Button mt={"4"} size={"2"}>
+            Read More
+          </Button>
+        </Box>
+      </Flex>
+    </Card>
+  );
+}
